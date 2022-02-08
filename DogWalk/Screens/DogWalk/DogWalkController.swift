b13c46e21b4a3ff346536963d6e49b1fad134cef
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-02-08 18:29:31 +0300

    Project initialization

---
+++ DogWalk/Screens/DogWalk/DogWalkController.swift
@@ -0,0 +1,17 @@
+//
+//  DogWalkController.swift
+//  DogWalk
+//
+//  Created by Stanislav Lemeshaev on 08.02.2022.
+//  Copyright © 2022 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class DogWalkController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
