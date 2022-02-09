d982e3f4b2d2e9f7e659add938592898faecbd66
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-02-09 21:01:09 +0300

    Added the screen with add time of walk

--- DogWalk/Screens/DogWalk/DogWalkController.swift
+++ DogWalk/Screens/DogWalk/DogWalkController.swift
@@ -10,8 +10,54 @@ import UIKit
 
 class DogWalkController: UIViewController {
     
+    private let identifier = "Cell"
+    
+    // MARK: - IBOutlets
+    @IBOutlet private var tableView: UITableView!
+    
+    // MARK: - Properties
+    private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    
+    private var walks: [Date] = []
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        configureUI()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func addWalk(_ sender: UIBarButtonItem) {
+        walks.append(Date())
+        tableView.reloadData()
+    }
+    
+    // MARK: - Private
+    private func configureUI() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension DogWalkController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return walks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        let date = walks[indexPath.row]
+        cell.textLabel?.text = dateFormatter.string(from: date)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Time of walks"
     }
 }
